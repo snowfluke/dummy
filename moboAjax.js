0a675add5c154cd55df6b538d90df6547345c0ce
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-08-21 21:22:02 +0700

    Create moboAjax.js

---
+++ moboAjax.js
@@ -0,0 +1,38 @@
+         $(document).ready(function(){ 
+ 
+              $('#Matherboard').keyup(function(){  
+                   var query = $(this).val();  
+                   if(query != '')  
+                   {  
+                        $.ajax({  
+                             url:"ajax/ajaxrakitan/Matherboard.php",  
+                             method:"POST",  
+                             data:{query:query},  
+                             success:function(data)  
+                             {  
+                                  $('#LMatherboard').fadeIn('fast');  
+                                  $('#LMatherboard').html(data);  
+                             },  
+                             error: function(jqXHR, textStatus, errorThrown)
+                             {
+                             	   $('#hMatherboard').val(0);
+                             }
+                        });  
+                   }  
+              });  
+              
+              $(document).on('click', '.Matherboard', function(){  
+                    $('#Matherboard').val($(this).children('#data-nama').text());
+                    $('#hMatherboard').val($(this).children('#data-harga2').text());
+                    
+                    total = total + parseInt($(this).children('#data-harga').text());
+                    
+                    $("#jmlttl").val(total);
+                    $('#LMatherboard').fadeOut('fast');
+              }); 
+              
+              $(document).on('click', function(){
+                   $('#LMatherboard').fadeOut('fast');  
+              }); 
+ 
+         }); 
